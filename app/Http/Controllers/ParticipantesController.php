d78f3b38a285f65d8ecd31f78870fbe04cda9064
lighthammer <jhoan.fury@gmail.com>
2023-06-08 16:20:59 -0500

    reload pdf qr para ingles js

--- app/Http/Controllers/ParticipantesController.php
+++ app/Http/Controllers/ParticipantesController.php
@@ -49,7 +49,7 @@ class ParticipantesController extends Controller
         $botones[3] = '<button disabled="disabled" style="margin-right: 5px;" class="btn btn-default btn-sm" id="ver-eventos"><img style="width: 19px; height: 20px;" src="'.URL::asset('images/iconos/lupa.png').'"><br>Ver Eventos</button>';
         $data["botones"] = $botones;
 
-        $data["scripts"] = $this->cargar_js(["participantes.js?version=180520232052"]);
+        $data["scripts"] = $this->cargar_js(["participantes.js?version=080620232052"]);
         return parent::init($view, $data);
 
 
